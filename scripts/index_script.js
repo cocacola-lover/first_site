5c81fa7a6a55d47de0c7e4bd4708a6b1141fa2df
cocacola-lover <pegas620570@gmail.com>
2022-08-25 10:27:11 +0300

    fixed button logic, made more adaptive

--- scripts/index_script.js
+++ scripts/index_script.js
@@ -1,38 +1,40 @@
 const btn = document.querySelector('.click');
 btn.style.minWidth = `0vw`;
 
-const startingSize = 7;
-const expandBy = 5;
-let size = startingSize;
+//const startingSize = 7;
+const startingSize = btn.offsetWidth * 100 / document.documentElement.offsetWidth;
+const expandBy = startingSize/2;
+let size = 0;
 let margin = 0;
 let expand = true;
+btn.style.width = `${startingSize + size * expandBy}vw`;
 
 btn.addEventListener("click", () => {
 
-    if (btn.style.marginLeft > "80vw") {
-        margin = -10 - size + startingSize/2;
-        btn.style.marginLeft = `${margin}vw`;
+    if (Number(btn.style.marginLeft.slice(0, -2)) > 85) {
+        margin = -10/expandBy -startingSize/expandBy/2;
+        btn.style.marginLeft = `${margin * expandBy}vw`;
         return;
     }
 
-    if (expand === true) {
-        if (size == startingSize + expandBy * 4) {
+    if (expand) {
+        if (size === 4) {
             expand = false;
-            margin += expandBy;
-            size -= expandBy;
+            margin++;
+            size--;
         }
-        else size += expandBy;
+        else size++;
     }
     else {
-        if (size == startingSize) {
+        if (size === 0) {
             expand = true;
-            size += expandBy;
+            size++;
         }
         else {
-            margin += expandBy;
-            size -= expandBy;
+            margin++;
+            size--;
         }
     }
-    btn.style.minWidth = `${size}vw`;
-    btn.style.marginLeft = `${margin}vw`;
+    btn.style.width = `${startingSize + size * expandBy}vw`;
+    btn.style.marginLeft = `${margin * expandBy}vw`;
 });
